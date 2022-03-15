db6525fc7c692dab48a859e22ca73666cfe3e25e
Steph <airth.steph@gmail.com>
2022-03-15 17:08:09 +0000

    update app.js
    call advice randomizer on page load as well as on button click, so users don't see the same quote on every pageload

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ var generatorBtn = document.querySelector(".generator"),
     adviceField = document.querySelector(".advice-text"), 
     adviceIdField = document.querySelector(".advice-num");
 
+document.onload = generateQuote();
+
 generatorBtn.addEventListener("click", function(){
-  var XHR = new XMLHttpRequest();
-  XHR.onreadystatechange = function(){
-    if(XHR.readyState == 4 && XHR.status == 200) {
-      var advice = JSON.parse(XHR.responseText).slip.advice;
-      var adviceId = JSON.parse(XHR.responseText).slip.id;
-      adviceField.innerText = advice;
-      adviceIdField.innerText = adviceId;
-    }
-  };
-   XHR.open("GET","https://api.adviceslip.com/advice");
-  XHR.send();
+  generateQuote()
 });
+
+function generateQuote() {
+    var XHR = new XMLHttpRequest();
+    XHR.onreadystatechange = function(){
+      if(XHR.readyState == 4 && XHR.status == 200) {
+        var advice = JSON.parse(XHR.responseText).slip.advice;
+        var adviceId = JSON.parse(XHR.responseText).slip.id;
+        adviceField.innerText = advice;
+        adviceIdField.innerText = adviceId;
+      }
+    };
+    XHR.open("GET","https://api.adviceslip.com/advice");
+    XHR.send();
+}
